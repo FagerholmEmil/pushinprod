ff5e826c2e048790defc8f5aaed155455b0a570b
FagerholmEmil <blazyvici@gmail.com>
2024-09-27 18:26:17 +0200

    backend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+      
+    </div>
+  );
+}
