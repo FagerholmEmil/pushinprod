ddec42b34da2d563aa88254fc4c83fae70530ead
FagerholmEmil <blazyvici@gmail.com>
2024-09-28 16:35:25 +0200

    reverted

--- src/app/[github-user]/[github-repo]/actions.ts
+++ src/app/[github-user]/[github-repo]/actions.ts
@@ -5,8 +5,7 @@ import path from 'path';
 
 export async function getFileData(githubUser: string, githubRepo: string) {
   const fileName = `${githubUser}-${githubRepo}.json`;
-  const _path = path.join('/tmp', 'knowledge-tree');
-  const filePath = path.join(_path, fileName)
+  const filePath = path.join(process.cwd(), 'knowledge-tree', fileName);
   const fileContent = fs.readFileSync(filePath, 'utf8');
   return JSON.parse(fileContent);
 }
