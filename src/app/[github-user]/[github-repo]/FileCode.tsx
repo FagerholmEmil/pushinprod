d25ba60bc06fc8b31963ded70dd12fa48003b4b8
imMatheus <matheusmendesbarata03@gmail.com>
2024-09-28 15:30:51 +0200

    changed empty state

--- src/app/[github-user]/[github-repo]/FileCode.tsx
+++ src/app/[github-user]/[github-repo]/FileCode.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import React from 'react';
-import { repoDataAtom, selectedFileAtom } from './state';
-import { useAtomValue } from 'jotai';
+import { fileExplorerOpenAtom, repoDataAtom, selectedFileAtom } from './state';
+import { useAtomValue, useSetAtom } from 'jotai';
 import { codeToHtml } from 'shiki';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { getLogo } from './getFileIcon';
 import { Skeleton } from '@/components/ui/skeleton';
+import { Button } from '@/components/ui/button';
 
 interface FileCodeProps {}
 
@@ -14,8 +15,23 @@ export const FileCode: React.FC<FileCodeProps> = ({}) => {
   const data = useAtomValue(repoDataAtom);
   const selectedFile = useAtomValue(selectedFileAtom);
   const file = selectedFile ? data[selectedFile as keyof typeof data] : null;
+  const setOpen = useSetAtom(fileExplorerOpenAtom);
 
-  if (!file || !selectedFile) return <p>No file selected</p>;
+  if (!file || !selectedFile)
+    return (
+      <div className="p-4">
+        <Button
+          variant="outline"
+          className="relative h-14 w-full justify-start rounded-[0.5rem] bg-muted/50 text-sm font-normal text-muted-foreground shadow-none w-full"
+          onClick={() => setOpen(true)}
+        >
+          Open a file
+          <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+            <span className="text-xs">⌘</span>K
+          </kbd>
+        </Button>
+      </div>
+    );
 
   const fileExtension = selectedFile.split('.').pop() || 'js';
 
