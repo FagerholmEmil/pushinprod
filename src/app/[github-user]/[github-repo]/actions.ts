69668c3570cf7538b5c76e6b2cc3cb5b9adc0ff7
FagerholmEmil <blazyvici@gmail.com>
2024-09-28 16:30:07 +0200

    change open script

--- src/app/[github-user]/[github-repo]/actions.ts
+++ src/app/[github-user]/[github-repo]/actions.ts
@@ -5,7 +5,8 @@ import path from 'path';
 
 export async function getFileData(githubUser: string, githubRepo: string) {
   const fileName = `${githubUser}-${githubRepo}.json`;
-  const filePath = path.join(process.cwd(), 'knowledge-tree', fileName);
+  const _path = path.join('/tmp', 'knowledge-tree');
+  const filePath = path.join(_path, fileName)
   const fileContent = fs.readFileSync(filePath, 'utf8');
   return JSON.parse(fileContent);
 }
